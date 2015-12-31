b7f3529444540b5642b58a48e8bc902fad8fb961
Junsheng <junsheng.guo90@gmail.com>
2015-12-30 23:09:54 -0800

    indention modified

--- java/WarmUp/src/MultipleRemoval/Node.java
+++ java/WarmUp/src/MultipleRemoval/Node.java
@@ -1,12 +1,12 @@
 package MultipleRemoval;
 
 public class Node {
-	public Node next;
-	public int val;
+  public Node next;
+  public int val;
 
-	public Node(int v) {
-		val = v;
-		next = null;
+  public Node(int v) {
+    val = v;
+    next = null;
 
-	}
+  }
 }
--- java/WarmUp/test/MultipleRemoval/ComponentTest.java
+++ java/WarmUp/test/MultipleRemoval/ComponentTest.java
@@ -3,55 +3,55 @@ package MultipleRemoval;
 import junit.framework.TestCase;
 
 public class ComponentTest extends TestCase {
-	public void testNode() throws Exception {
-		Node node = new Node(10);
-		assertEquals(10, node.val);
-		assertEquals(null, node.next);
-		node.next = node;
-		assertEquals(node, node.next);
-	}
-	
-	public void testRemovalRequest() throws Exception {
-		RemovalRequest rr = new RemovalRequest(10, 100);
-		assertEquals(10, rr.pos);
-		assertEquals(100, rr.num);
-	}
-	
-	public void testRemove() throws Exception {
-		Node n1 = new Node(1);
-		Node n2 = new Node(2);
-		Node n3 = new Node(3);
-		Node n4 = new Node(4);
-		Node n5 = new Node(5);
-		Node n6 = new Node(6);
+  public void testNode() throws Exception {
+    Node node = new Node(10);
+    assertEquals(10, node.val);
+    assertEquals(null, node.next);
+    node.next = node;
+    assertEquals(node, node.next);
+  }
 
-		n1.next = n2;
-		n2.next = n3;
-		n3.next = n4;
-		n4.next = n5;
-		n5.next = n6;
-		
-		RemovalRequest rr1 = new RemovalRequest(0, 1);
-		RemovalRequest rr2 = new RemovalRequest(3, 2);
-		RemovalRequest rr3 = new RemovalRequest(4, 1);
-		RemovalRequest[] rrs = new RemovalRequest[3];
-		rrs[0] = rr1;
-		rrs[1] = rr2;
-		rrs[2] = rr3;
-		
-		int[] expected_a = {2, 3, 6};
-		
-		RemoveNodes rn = new RemoveNodes();
-		Node res = rn.Remove(n1, rrs);
-		int i = 0;
-		while (res != null && i < expected_a.length) {
-			assertEquals(expected_a[i], res.val);
-			res = res.next;
-			++i;
-		}
-		
-		if (res != null || i < expected_a.length) {
-			fail();
-		}
-	}
+  public void testRemovalRequest() throws Exception {
+    RemovalRequest rr = new RemovalRequest(10, 100);
+    assertEquals(10, rr.pos);
+    assertEquals(100, rr.num);
+  }
+
+  public void testRemove() throws Exception {
+    Node n1 = new Node(1);
+    Node n2 = new Node(2);
+    Node n3 = new Node(3);
+    Node n4 = new Node(4);
+    Node n5 = new Node(5);
+    Node n6 = new Node(6);
+
+    n1.next = n2;
+    n2.next = n3;
+    n3.next = n4;
+    n4.next = n5;
+    n5.next = n6;
+
+    RemovalRequest rr1 = new RemovalRequest(0, 1);
+    RemovalRequest rr2 = new RemovalRequest(3, 2);
+    RemovalRequest rr3 = new RemovalRequest(4, 1);
+    RemovalRequest[] rrs = new RemovalRequest[3];
+    rrs[0] = rr1;
+    rrs[1] = rr2;
+    rrs[2] = rr3;
+
+    int[] expected_a = { 2, 3, 6 };
+
+    RemoveNodes rn = new RemoveNodes();
+    Node res = rn.Remove(n1, rrs);
+    int i = 0;
+    while (res != null && i < expected_a.length) {
+      assertEquals(expected_a[i], res.val);
+      res = res.next;
+      ++i;
+    }
+
+    if (res != null || i < expected_a.length) {
+      fail();
+    }
+  }
 }
